e82311a77c8e9fb142945e24e2982a795a2f37cf
Domannaa <43107927+Domannaa@users.noreply.github.com>
2019-02-14 15:06:00 +1100

    Update Class - Factotum.js

--- Class - Factotum.js
+++ Class - Factotum.js
@@ -22,7 +22,7 @@ ClassList["factotum"] = {
 	abilitySave : 4,
 	skills : ["\n\n" + toUni("Factotum") + ": Choose any three skills.", "\n\n" + toUni("Factotum") + ": Choose any one skill."],
 	toolProfs : {
-		primary : [["Any tool", 4]],
+		primary : [["Any tool", 3]],
 		secondary : [["Any tool", 1]]
 	},
 	armor : [
@@ -40,7 +40,7 @@ ClassList["factotum"] = {
 		"jack of all trades" : { // feature object names should all be lowercase
 			name : "Jack of All Trades",
 			source : ["D:FC", 0],
-			minlevel : 2,
+			minlevel : 1,
 			description : "\n   " + "I can add half my proficiency bonus to any ability check that doesn't already include it",
 			eval : "Checkbox('Jack of All Trades', true);",
 			removeeval : "Checkbox('Jack of All Trades', false);"
@@ -60,9 +60,9 @@ ClassList["factotum"] = {
 			source : ["D:FC", 0],
 			minlevel : 2,
 			description : desc([
-					"I gain inspiration points, which I can spend on various abilities",
-					"as a bonus action, I can spend one to add my Int bonus to an attack,",
-					"check or save, or I can dash or disengage"
+				"I gain inspiration points, which I can spend on various abilities",
+				"The amount of points i have is equal to my level, and recovers on a short rest",
+				"as a bonus action, I can spend 1 to add my Int bonus to an attack, check or save"
 				]),
 			action : ["bonus action", ""],
 			usages : [0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
@@ -73,8 +73,20 @@ ClassList["factotum"] = {
 			source : ["D:FC", 0],
 			minlevel : 2,
 			description : desc([
-					"I use an action to cast a spell from the wizard list up to 1st level using genius points equal to spell lvl",
-					"I can cast 2nd level spells at 5th level, 3rd at 8th, 4th at 10th, 5th at 13th"
+				"I use an action to cast a wizard spell up to 1st level using genius points equal to spell lvl",
+				"I can cast 2nd level spells at 5th level, 3rd at 9th, 4th at 13th, 5th at 17th"
+				]),
+			action : ["action", ""]
+		},
+		"opportunistic piety" : {
+			name : "Opportunistic Piety",
+			source : ["D:FC", 0],
+			minlevel : 3,
+			description : desc([
+				"As an action, i spend an inspiration point to make an expedient plea for divine aid.",
+				"Healing Touch: Heal a creature i touch by my factotum level + Int modifier,",
+				"Repel the Dead: Force all undead in 30ft to make a wisdom saving throw vs my DC.",
+				"If they fail this save, they are turned, and can only spend their turns moving away from me"
 				]),
 			action : ["action", ""]
 		},
@@ -84,22 +96,10 @@ ClassList["factotum"] = {
 			minlevel : 3,
 			description : "\n   " + "Choose a Archetype you strive to emulate and put it in the \"Class\" field" + "\n   " + "Choose Martial Savant"
 		},
-		"opportunistic piety" : {
-			name : "Opportunistic Piety",
-			source : ["D:FC", 0],
-			minlevel : 3,
-			description : desc([
-					"As an action, I can appeal to the gods in a spur of the moment plea of desperation. I can either:",
-					"Spend a genius point to heal a creature I touch by my factotum level + Int modifier,",
-					"or spend a genius point to force all undead in 30ft to make a wisdom saving throw vs my DC",
-					"If they fail this save, they are turned, and can only spend their turns moving away from me"
-				]),
-			action : ["action", ""]
-		},
 		"brains over brawn" : {
 			name : "Brains over Brawn",
 			souce : ["D:FC", 0],
-			minlevel : 5,
+			minlevel : 6,
 			description : desc([
 					"I can substitute Intelligence for Strength, Dexterity or Constitution in a check"
 				])
@@ -107,18 +107,18 @@ ClassList["factotum"] = {
 		"cunning surge" : {
 			name : "Cunning Surge",
 			source : ["D:FC", 0],
-			minlevel : 9,
+			minlevel : 10,
 			description : desc([
 					"I can spend 5 Inspiration points to take an extra action on my turn"
 				])
 		},
-		"arcane understanding" : {
-			name : "Arcane Understanding",
+		"mind over matter" : {
+			name : "Mind over Matter",
 			source : ["D:FC", 0],
-			minlevel : 15,
+			minlevel : 14,
 			description : desc([
-					"My knowledge of the arcane broadens, allowing me to cast greater spells using Mystical Dilettante",
-					"Once per long rest, I can cast a 6th level spell, or a 7th level spell at 18th level"
+					"Once per long rest, i can add my intelligence modifier to a save that i fail as a reaction",
+					"At 18th Level, the save also becomes an int saving throw, and i re-roll choosing the higher."
 				])
 		},
 		"spark of brilliance" : {
@@ -164,16 +164,16 @@ AddSubClass(
 		"subclassfeature7" : {
 			name : "Extra Attack",
 			source : ["D:FC", 0],
-			minlevel : 7,
+			minlevel : 5,
 			description : desc([
-					"I can attack twice when I take the attack action, and 3 times at 14th level"
+					"I can attack twice when I take the attack action, and 3 times at 11th level"
 				])
-			
+
 		},
 		"subclassfeature11" : {
 			name : "Chink in the Armor",
 			source : ["D:FC", 0],
-			minlevel : 11,
+			minlevel : 7,
 			description : desc([
 					"When I use my Pinpoint strike, I ignore resistance, and treat immunity as resistance"
 				])
@@ -181,7 +181,7 @@ AddSubClass(
 		"subclassfeature17" : {
 			name : "Calculated Blow",
 			source : ["D:FC", 0],
-			minlevel : 17,
+			minlevel : 15,
 			description : desc([
 					"When making an attack, I can spend 3 inspiration points to gain advantage on the attack roll",
 					"If I hit with this attack, damage is treated as if the maximum was rolled on the weapon's die"
